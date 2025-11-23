3cee04837d02e5c7ff4c20aafedd35ed42227bf7
Isaaja <isaiantmaulana2004@gmail.com>
2025-11-23 17:44:52 +0700

    Clean Architecture - Dicoding Expert

---
+++ src/Interfaces/http/api/users/handler.js
@@ -0,0 +1,35 @@
+const AddUserUseCase = require("../../../../Applications/use_case/AddUserUseCase");
+const DomainErrorTranslator = require("../../../../Commons/exceptions/DomainErrorTranslator");
+
+class UsersHandler {
+  constructor(container) {
+    this._container = container;
+
+    this.postUserHandler = this.postUserHandler.bind(this);
+  }
+
+  async postUserHandler(request, h) {
+    try {
+      const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
+      const addedUser = await addUserUseCase.execute(request.payload);
+      const response = h.response({
+        status: "success",
+        data: {
+          addedUser,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      const translatedError = DomainErrorTranslator.translate(error);
+      const response = h.response({
+        status: "fail",
+        message: translatedError.message,
+      });
+      response.code(translatedError.statusCode);
+      return response;
+    }
+  }
+}
+
+module.exports = UsersHandler;
